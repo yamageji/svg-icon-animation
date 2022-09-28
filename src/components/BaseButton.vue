1aafd09a9db97c07f602cb1a6999171cea880316
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-09-28 23:12:20 +0900

    fix: add button attributes

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -11,6 +11,7 @@ const handleClick = () => {
 
 <template>
   <button
+    type="button"
     class="group inline-flex items-center gap-2 rounded-full bg-gray-700 px-4 py-1.5 font-semibold text-white hover:bg-gray-900"
     @click="handleClick"
   >
